35a3360604ea5b596b2521f1b986490ec04f088e
jeffnyak@gmail.com <jeffnyak@gmail.com>
2022-06-29 22:12:23 +0300

    Login with JWT brearer

--- Controllers/AuthenticationController.cs
+++ Controllers/AuthenticationController.cs
@@ -1,9 +1,13 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using PropertiesListings.Authentication;
 using PropertiesListings.Dtos;
 using PropertiesListings.Helpers;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace PropertiesListings.Controllers
 {
@@ -13,12 +17,14 @@ namespace PropertiesListings.Controllers
     {
         private readonly UserManager<ApplicationUser> userManager;
         private readonly RoleManager<IdentityRole> roleManager;
+        private readonly IConfiguration configuration;
 
         public AuthenticationController(UserManager<ApplicationUser> userManager, 
-            RoleManager<IdentityRole> roleManager)
+            RoleManager<IdentityRole> roleManager, IConfiguration configuration)
         {
             this.userManager = userManager;
             this.roleManager = roleManager;
+            this.configuration = configuration;
         }
         //Register user
         [HttpPost]
@@ -54,6 +60,55 @@ namespace PropertiesListings.Controllers
                     );
             }
             return Ok(new UserManagerResponse {Status = "Success", Message="Created successfully", IsSuccess = true });
+        }
+
+        //User login
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login([FromBody] LoginDto loginDto)
+        {
+            var user = await userManager.FindByEmailAsync(loginDto.Email);
+
+            if (user == null)
+            {
+                return StatusCode(StatusCodes.Status400BadRequest,
+                    new UserManagerResponse { Status = "Error", Message = "User with email does not exist", IsSuccess = false }
+                    );
+            }
+
+            var checkPwd = await userManager.CheckPasswordAsync(user, loginDto.Password);
+
+            if (user != null && checkPwd)
+            {
+               var claims = new[]
+               {
+                    new Claim("Email", loginDto.Email),
+                    new Claim(ClaimTypes.NameIdentifier, user.Id)
+                };
+                var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Secret"]));
+                var token = new JwtSecurityToken(
+                    issuer: configuration["JWT:ValidIssuer"],
+                    audience: configuration["JWT:ValidAudience"],
+                    claims: claims,
+                    expires: DateTime.Now.AddHours(2),
+                    signingCredentials: new SigningCredentials(key, SecurityAlgorithms.HmacSha256)
+                );
+                string tokenString = new JwtSecurityTokenHandler().WriteToken(token);
+                return Ok(new UserManagerResponse
+                {
+                    Message = tokenString,
+                    IsSuccess = true,
+                    ExpireDate = token.ValidTo
+                });
+            }
+            else
+            {
+                return Ok(new UserManagerResponse
+                {
+                    Message = "Invalid email or password",
+                    IsSuccess = false
+                });
+            }
         }
     }
 }
--- Helpers/UserManagerResponse.cs
+++ Helpers/UserManagerResponse.cs
@@ -2,9 +2,10 @@
 {
     public class UserManagerResponse
     {
-        public string Message { get; set; }
+        public string? Message { get; set; }
         public bool IsSuccess { get; set; }
-        public string Status { get; set; }
-        public IEnumerable<string> errors { get; set; }
+        public string? Status { get; set; }
+        //public IEnumerable<string>? errors { get; set; }
+        public DateTime? ExpireDate { get; set; }
     }
 }
